cf63b22a15b1d16db6e6a5ce27cb2bf265105b49
Illzen <908413385@qq.com>
2023-10-13 08:07:10 +0800

    fix .sql

--- sql/createTbale.sql
+++ sql/createTbale.sql
@@ -1,13 +1,15 @@
-CREATE TABLE districts { adcode INT,
-name VARCHAR(20),
-PRIMARY KEY(adcode) };
+CREATE TABLE districts (
+	adcode INT,
+	name VARCHAR(20),
+	PRIMARY KEY(adcode)
+);
 
-CREATE TABLE specialty { id INT,
-name VARCHAR(20),
-PRIMARY KEY(id) };
+CREATE TABLE specialty (id INT, name VARCHAR(20), PRIMARY KEY(id));
 
-CREATE TABLE regional_specialties { adcode INT,
-s_id INT,
-FOREIGN KEY (adcode) REFERENCES districts (adcode),
-FOREIGN KEY (s_id) REFERENCES specialty (id),
-PRIMARY KEY(d_id, s_id) }
+CREATE TABLE regional_specialties (
+	adcode INT,
+	s_id INT,
+	FOREIGN KEY (adcode) REFERENCES districts (adcode),
+	FOREIGN KEY (s_id) REFERENCES specialty (id),
+	PRIMARY KEY(d_id, s_id)
+)
